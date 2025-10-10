eb39d9d36871498cc09c9a350d0141d459f7f110
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-10 09:06:54 +0700

    Create Lớp Nhân Viên 1.java

---
+++ Lớp Nhân Viên 1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+class nhanvien{
+    private String ma,ten,gt,ns,dc,mst,nkhd;
+    nhanvien(){}
+    public nhanvien(String ma, String ten, String gt, String ns, String dc, String mst, String nkhd){
+        this.ma = ma;
+        this.ten = ten;
+        this.gt = gt;
+        this.ns = ns;
+        this.dc = dc;
+        this.mst = mst;
+        this.nkhd = nkhd;
+    }
+    public void chuanHoaNS(){
+        String []arr = ns.split("/");
+        if(arr[0].length() == 1){
+            arr[0] = "0" + arr[0];
+        }
+        if(arr[1].length() == 1){
+            arr[1] = "0" + arr[1];
+        }
+        ns = arr[0] + "/" + arr[1] + "/" + arr[2];
+    }
+    public void chuanHoaNKHD(){
+        String []arr = nkhd.split("/");
+        if(arr[0].length() == 1){
+            arr[0] = "0" + arr[0];
+        }
+        if(arr[1].length() == 1){
+            arr[1] = "0" + arr[1];
+        }
+        nkhd = arr[0] + "/" + arr[1] + "/" + arr[2];
+    }
+    public void in(){
+        System.out.printf("%s %s %s %s %s %s %s",ma,ten,gt,ns,dc,mst,nkhd);
+    }
+}
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        nhanvien nv = new nhanvien("00001",sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine());
+        nv.chuanHoaNKHD(); nv.chuanHoaNS();
+        nv.in();
+    }
+}
