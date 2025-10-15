2d52b8a4dc1b507897837062d7a78dff80664270
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-15 08:14:20 +0700

    Create  Lớp ma trận 2.java

---
+++ Lớp ma trận 2.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+class matrix{
+    private int [][]v;
+    private int hang,cot;
+    public matrix(int hang,int cot, Scanner sc){
+        this.hang = hang;
+        this.cot = cot;
+        v = new int[hang][cot];
+        for(int i = 0;i < hang;i++){
+            for(int j = 0;j < cot;j++){
+                v[i][j] = sc.nextInt();
+            }
+        }
+    }
+    public matrix(int hang,int cot){
+        this.hang = hang;
+        this.cot = cot;
+        v = new int[hang][cot];
+    }
+    public matrix trans(){
+        matrix tmp = new matrix(hang,cot);
+        tmp.hang = cot;
+        tmp.cot = hang;
+        tmp.v = new int[cot][hang];
+        for(int i = 0; i < hang;i++){
+            for(int j = 0;j < cot;j++){
+                tmp.v[j][i] = v[i][j];
+            }
+        }
+        return tmp;
+    }
+    public matrix mul(matrix other){
+        int n = hang, m = cot, p = other.cot;
+        matrix tmp = new matrix(n,p);
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < p;j++){
+                for(int k = 0;k < m;k++){
+                    tmp.v[i][j] += v[i][k] * other.v[k][j];
+                }
+            }
+        }
+        return tmp;
+    }
+    public void print(){
+        for(int i = 0;i < hang;i++){
+            for(int j = 0;j < cot;j++){
+                System.out.print(v[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        int n = sc.nextInt(), m = sc.nextInt();
+        matrix a = new matrix(n,m,sc);
+        matrix b = a.trans();
+        matrix c = a.mul(b);
+        c.print();
+    }
+}
