2b91694dd1084e204590ecf998b3263f24a46f05
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-20 08:47:55 +0700

    Create Sắp xếp sinh viên theo lớp.java

---
+++ Sắp xếp sinh viên theo lớp.java
@@ -0,0 +1,48 @@
+import jdk.jshell.Snippet;
+
+import java.io.*;
+import java.util.*;
+class sinhVien{
+    private String ma,ten,lop,email;
+
+    public sinhVien(String ma, String ten, String lop, String email) {
+        this.ma = ma;
+        this.ten = ten;
+        this.lop = lop;
+        this.email = email;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+    public void print(){
+        System.out.println(ma + " " + ten + " " + lop + " " + email);
+    }
+}
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(sc.nextLine());
+        Vector<sinhVien> v = new Vector<>();
+        for(int i = 0;i < n;i++){
+            sinhVien sv = new sinhVien(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine());
+            v.add(sv);
+        }
+        Collections.sort(v, new Comparator<sinhVien>() {
+            @Override
+            public int compare(sinhVien o1, sinhVien o2) {
+                if (o1.getLop().equals(o2.getLop())) {
+                    return o1.getMa().compareTo(o2.getMa());
+                }
+                return o1.getLop().compareTo(o2.getLop());
+            }
+        });
+        for(sinhVien x : v){
+            x.print();
+        }
+    }
+}
